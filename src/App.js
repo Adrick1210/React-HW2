d1798f48a470f296abb773e4e4efea1bfa6d2afa
Christian Alexander <christian.alexanderato@gmail.com>
2024-01-27 08:32:31 -0800

    new component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import Movies from './components/Movies';
 
 function App() {
+  const movies = [
+    {
+      name: "Scott Pilgrim Vs The World",
+      year: "2010",
+      rating: "PG-13",
+      image: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p8045259_p_v12_an.jpg"
+
+    },
+    {
+      name: "The Dark Knight",
+      year: "2008",
+      rating: "R",
+      image: "https://upload.wikimedia.org/wikipedia/en/1/1c/The_Dark_Knight_%282008_film%29.jpg"
+
+    },
+    {
+      name: "Wonka",
+      year: "2023",
+      rating: "PG",
+      image: "https://amc-theatres-res.cloudinary.com/image/upload/f_auto,fl_lossy,h_465,q_auto,w_310/v1699288064/amc-cdn/production/2/movies/65900/65858/PosterDynamic/160145.jpg"
+
+    },
+    {
+      name: "Avatar: The Way of Water",
+      year: "2022",
+      rating: "PG-13",
+      image: "https://lumiere-a.akamaihd.net/v1/images/p_disneymovies_avatarthewayofwater_streamingupdate_2096_0908fa1b.jpeg"
+
+    },
+    {
+      name: "Ferris Bueller's Day Off",
+      year: "1986",
+      rating: "PG-13",
+      image: "https://m.media-amazon.com/images/M/MV5BMDA0NjZhZWUtNmI2NC00MmFjLTgwZDYtYzVjZmNhMDVmOTBkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg"
+
+    },
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        
     </div>
   );
 }
---
+++ src/components/Movies.js
@@ -0,0 +1,8 @@
+// the component function
+const Movies = (props) => {
+  //The Components Returned JSX
+  return <div className="ComponentName"> This is movies</div>;
+};
+
+// export the component
+export default Movies;
